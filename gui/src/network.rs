9f99e25d13b4c59b5aebd0e63351d4301680fcc6
axelamit <axel.v.nilsson@gmail.com>
2020-10-08 20:48:05 +0200

    add basic parsing and encoding

--- gui/src/network.rs
+++ gui/src/network.rs
@@ -1,8 +1,32 @@
+use chess_engine::pieces::default::*;
+use chess_engine::Board;
+use chess_engine::Team;
+
 use std::thread;
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
 use std::str::from_utf8;
 use std::thread::{spawn, JoinHandle};
+
+pub enum Move {
+    Standard(i64, i64),
+    EnPassant(i64, i64),
+    Promotion(i64, i64, i64),
+    KingsideCastling,
+    QueensideCastling,
+    Other,
+}
+
+pub enum MessageType {
+    Decline,
+    Move(Move),
+    Undo,
+    Accept,
+    Checkmate,
+    Draw, 
+    Resign, 
+    Other,
+}
 
 pub struct Net{
     host: bool,
@@ -89,5 +113,40 @@ impl Net{
                 stream.shutdown(Shutdown::Both).unwrap();
             }
         }
+    }
+}
+
+pub fn parse_message(message: [u8, 32]) -> String{
+    match message[0] {
+        0 => MessageType::Decline, 
+        1 => {
+            MessageType::Move(match message[1] {
+                0 => Move::Standard(message[2], message[3]),
+                1 => Move::Standard(message[2], message[3]),
+                2 => Move::Promotion(message[2], message[3], message[4]),
+                3 => Move::KingsideCastling,
+                4 => Move::QueensideCastling,
+                _ => Move::Other,
+            })
+        }
+        2 => MessageType::Undo,
+        3 => MessageType::Accept,
+        4 => MessageType::Checkmate,
+        5 => MessageType::Draw,
+        6 => MessageType::Resign,
+        _ => MessageType::Other,
+    }
+}
+
+pub fn encode_message(type: MessageType) {
+    match type{
+        Decline => 0, 
+        Move() => 1,
+        Undo => 2, 
+        Accept => 3,
+        Checkmate => 4,
+        Draw => 5,
+        Resign => 6,
+        _ => -1, 
     }
 }
